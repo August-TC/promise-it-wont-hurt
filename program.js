ee05112fc3add4e5d9cc6e039dc6b7b71ddffd98
august <august_tancheng@hotmail.com>
2017-10-17 10:24:23 +0800

    Modify 2017-10-17

---
+++ program.js
@@ -0,0 +1,12 @@
+'use strict'
+
+function output(str) {
+	console.log(str);
+}
+
+/**
+ *  setTimeout(code, ms)
+ *  code: JavaScript codes in a string
+ *  ms: delay time
+ */
+setTimeout(output,300,"TIMED OUT!");
